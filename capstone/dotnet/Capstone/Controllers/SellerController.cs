dce26e32f522e7645f3dcf1ea0c47903798363af
Matt karp <mattk.biochem@gmail.com>
2023-08-09 11:16:51 -0400

    8/7/2023 finale capstone

---
+++ capstone/dotnet/Capstone/Controllers/SellerController.cs
@@ -0,0 +1,81 @@
+﻿using Capstone.DAO;
+using Microsoft.AspNetCore.Mvc;
+using Capstone.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Capstone.Exceptions;
+
+namespace Capstone.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin")]
+    public class EventsController : Controller
+    {
+        public IEventsDao eventsDao;
+
+        public EventsController(IEventsDao eventsDao)
+        {
+            this.eventsDao = eventsDao;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult<List<Events>> GetEvents()
+        {
+            return Ok(eventsDao.GetEvents());
+        }
+
+        [HttpGet("{eventsId}")]
+
+        public ActionResult<Events> GetEventsById(int eventsId)
+        {
+            return Ok(eventsDao.GetEventById(eventsId));
+        }
+
+        [HttpGet("future")] //getFutureEvents()
+        public ActionResult<List<Events>> GetFutureEvents()
+        {
+            return Ok(eventsDao.GetFutureEvents());
+        }
+
+        [HttpPut("{id}")]
+
+        public ActionResult<Events> UpdateEvent(int id, Events eventToUpdate)
+        {
+            eventToUpdate.EventId = id;
+
+            try
+            {
+                Events result = eventsDao.UpdateEvent(eventToUpdate);
+                return Ok(result);
+            }
+            catch (DaoException)
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Events> DeleteEvent(int id)
+        {
+            bool isDeleted = eventsDao.DeleteEvent(id);
+            if (isDeleted)
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpPost()]
+        public ActionResult<Events> AddEvent(Events newEvent)
+        {
+            Events added = eventsDao.AddEvent(newEvent);
+            return Created($"/events/{added.EventId}", added);
+        }
+    }
+
+
+}
