bc33eee421c3ffae11707051986a08e4c12386c4
Emily Bates <emilyspbates@gmail.com>
2023-08-09 20:31:28 -0400

    capstone css styling updates

---
+++ capstone/vue/src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <header id="header">
+    <h1>HeaderComponent.vue</h1>
+
+    <div class="login-out">
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link>
+      <router-link v-bind:to="{ name: 'login' }" v-else>Login</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
