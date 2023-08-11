b87cb87b01795293412a52d451b513f65990f418
Mitchell Essig <mitch.essig@gmail.com>
2023-08-11 12:21:33 -0400

    notification component and icons

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="home">
+    <notification-display />
     <h1>Home</h1>
     <p>You are logged in</p>
   </div>
 </template>
 
 <script>
+import NotificationDisplay from '../components/NotificationDisplay.vue';
 export default {
-  components: {  },
+  components: {NotificationDisplay  },
   name: "home"
 };
 </script>
