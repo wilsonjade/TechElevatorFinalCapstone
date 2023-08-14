944e06ad14e24e5f31b2c3c0742cbd0aa17d567e
Jade Wilson <jadexowilson@gmail.com>
2023-08-14 10:58:50 -0400

    select menu for registration

--- capstone/vue/src/views/Register.vue
+++ capstone/vue/src/views/Register.vue
@@ -7,7 +7,13 @@
       </div>
       <div class="form-input-group">
         <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
+        <input
+          type="text"
+          id="username"
+          v-model="user.username"
+          required
+          autofocus
+        />
       </div>
       <div class="form-input-group">
         <label for="password">Password</label>
@@ -15,71 +21,128 @@
       </div>
       <div class="form-input-group">
         <label for="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" v-model="user.confirmPassword" required />
+        <input
+          type="password"
+          id="confirmPassword"
+          v-model="user.confirmPassword"
+          required
+        />
       </div>
-       <div class="form-input-group">
+
+      <div class="form-input-group">
         <label for="expertiseLevel">Level of Expertise</label>
-        <input type="number" id="expertiseLevel" v-model.number="user.expertiseLevel" required autofocus />
+        <select
+          name="expertiseLevel"
+          input
+          type="number"
+          id="expertiseLevel"
+          v-model.number="user.expertiseLevel"
+          required
+          autofocus
+        >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+
+        </select>
       </div>
+
       <div class="form-input-group">
         <label for="firstName">First Name</label>
-        <input type="text" id="firstName" v-model="user.firstName" required autofocus />
+        <input
+          type="text"
+          id="firstName"
+          v-model="user.firstName"
+          required
+          autofocus
+        />
       </div>
       <div class="form-input-group">
         <label for="lastName">Last Name</label>
-        <input type="text" id="lastName" v-model="user.lastName" required autofocus />
+        <input
+          type="text"
+          id="lastName"
+          v-model="user.lastName"
+          required
+          autofocus
+        />
       </div>
       <div class="form-input-group">
         <label for="email">Email</label>
         <input type="text" id="email" v-model="user.email" required autofocus />
       </div>
+
+
+
       <div class="form-input-group">
         <label for="region">Region</label>
-        <input type="number" id="region" v-model.number="user.region" required autofocus />
+        <select
+          name = "region"
+          input type="number"
+          id="region"
+          v-model.number="user.region"
+          required
+          autofocus
+        >
+
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+        <option>8</option>
+        <option>9</option>
+        <option>10</option>
+
+        </select>
       </div>
+
       <button type="submit">Create Account</button>
-      <p><router-link :to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
+      <p>
+        <router-link :to="{ name: 'login' }"
+          >Already have an account? Log in.</router-link
+        >
+      </p>
     </form>
   </div>
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
 
 export default {
-  name: 'register',
+  name: "register",
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
-        firstName: '', 
-        lastName: '', 
-        expertiseLevel: 0, 
-        email: '',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "user",
+        firstName: "",
+        lastName: "",
+        expertiseLevel: 0,
+        email: "",
         region: 0,
-
-
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -87,14 +150,14 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
   },
 };
