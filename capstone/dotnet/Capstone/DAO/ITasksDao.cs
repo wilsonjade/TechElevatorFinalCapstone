5ac15dd771224c6b5ec6e7e3f5ff4d24dbbde347
Mitchell Essig <mitch.essig@gmail.com>
2023-08-14 11:56:08 -0400

    server side fixes for Tasks dao

--- capstone/dotnet/Capstone/DAO/ITasksDao.cs
+++ capstone/dotnet/Capstone/DAO/ITasksDao.cs
@@ -13,29 +13,7 @@
         /// <returns>Full List of Events</returns>
         public List<Tasks> GetTasks();
 
-        public Events GetEventById(int id);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns>Full List of events in the future</returns>
-        public List<Events> GetFutureEvents();
-        /// <summary>
-        /// Insert event record to the database
-        /// </summary>
-        /// <param name="eventToAdd">Event object to insert</param>
-        /// <returns>Inserted Event object</returns>
-        public Events AddEvent(Events eventToAdd);
-        /// <summary>
-        /// Update an existing event record
-        /// </summary>
-        /// <param name="eventToUpdate">Event object</param>
-        /// <returns>The updated Event object from the database</returns>
-        public Events UpdateEvent(Events eventToUpdate);
-        /// <summary>
-        /// Deletes an event record
-        /// </summary>
-        /// <param name="eventId">Integer id of the event</param>
-        /// <returns>Boolean representing success or failure</returns>
-        public bool DeleteEvent(int eventId);
+        
+        public bool DeleteTask(int taskId);
     }
 }
